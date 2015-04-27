1c746c43044aa59c9d2e5683dd10c544a5b89817
charliektm400exc <charlie.franklin@me.com>
2015-04-27 10:14:05 +1000

    Created one container

---
+++ Lemonade Stand/Setting up Functions.swift
@@ -0,0 +1,12 @@
+//
+//  Setting up Functions.swift
+//  Lemonade Stand
+//
+//  Created by Charlie Franklin on 27/04/2015.
+//  Copyright (c) 2015 Charlie Franklin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
--- Lemonade Stand/ViewController.swift
+++ Lemonade Stand/ViewController.swift
@@ -9,17 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // Containers
+    
+    var firstContainer: UIView!
+    var secondContainer: UIView!
+    var thirdContainer: UIView!
+    var fourthContainer: UIView!
+    
+    //sizes
+    
+    let kMarginForView:CGFloat = 10
+    let kQuarter:CGFloat = 1.0/6.0
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+           self.setupContainerViews()}
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    func setupContainerViews () {
+    self.firstContainer = UIView(frame: CGRect(
+        x: self.view.bounds.origin.x + kMarginForView,
+        y: self.view.bounds.origin.y,
+        width: self.view.bounds.width - kMarginForView * 2,
+        height: self.view.bounds.height * kQuarter))
+    self.firstContainer.backgroundColor = UIColor.redColor()
+    self.view.addSubview(self.firstContainer)
+    }
 
 }
 
